4b9467f8a3f1c4b5ff35a0c5baa5046a2d5bceff
Michal Urbanski <m.urbanski@futuremind.com>
2020-01-03 19:45:14 +0100

    fix: concatenating tuple (ref. https://stackoverflow.com/q/49085230/7085690)

--- djcelery/management/commands/celery.py
+++ djcelery/management/commands/celery.py
@@ -11,11 +11,11 @@ base = celery.CeleryCommand(app=app)
 class Command(CeleryCommand):
     """The celery command."""
     help = 'celery commands, see celery help'
-    options = (
-        tuple(CeleryCommand.options) +
-        tuple(base.get_options() or ()) +
-        tuple(getattr(base, 'preload_options', ()))
-    )
+    # options = (
+    #     tuple(CeleryCommand.options) +
+    #     tuple(base.get_options() or ()) +
+    #     tuple(getattr(base, 'preload_options', ()))
+    # )
 
     def run_from_argv(self, argv):
         argv = self.handle_default_options(argv)
